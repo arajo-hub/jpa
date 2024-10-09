18d88a042e14bca71abe684c77586b6eb55404f4
Ara Jo <joara9566@naver.com>
2024-10-09 10:20:17 +0900

    Member 클래스 생성

---
+++ src/main/java/com/judy/jpa/chapter2/domain/Member.java
@@ -0,0 +1,14 @@
+package com.judy.jpa.chapter2.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Member {
+
+    private String id;
+    private String username;
+    private Integer age;
+
+}
