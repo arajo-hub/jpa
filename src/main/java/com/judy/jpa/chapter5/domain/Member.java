1badd6eab0dc7681141640339771b18052546abd
Ara Jo <joara9566@naver.com>
2024-10-17 22:32:07 +0900

    객체 관계 매핑

--- src/main/java/com/judy/jpa/chapter5/domain/Member.java
+++ src/main/java/com/judy/jpa/chapter5/domain/Member.java
@@ -1,15 +1,24 @@
 package com.judy.jpa.chapter5.domain;
 
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class Member {
 
+    @Id
+    @Column(name = "MEMBER_ID")
     private String id;
+
     private String username;
 
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
     private Team team;
 
     public Member(String id, String username) {
--- src/main/java/com/judy/jpa/chapter5/domain/Team.java
+++ src/main/java/com/judy/jpa/chapter5/domain/Team.java
@@ -1,13 +1,22 @@
 package com.judy.jpa.chapter5.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@NoArgsConstructor
 public class Team {
 
+    @Id
+    @Column(name = "TEAM_ID")
     private String id;
+
     private String name;
 
     public Team(String id, String name) {
