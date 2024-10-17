004c0f6fa0992eebabcb8d573290e41dec95fe5b
Ara Jo <joara9566@naver.com>
2024-10-17 22:44:57 +0900

    회원과 팀을 저장하는 코드

--- src/main/java/com/judy/jpa/chapter5/JpaMain.java
+++ src/main/java/com/judy/jpa/chapter5/JpaMain.java
@@ -2,20 +2,43 @@ package com.judy.jpa.chapter5;
 
 import com.judy.jpa.chapter5.domain.Member;
 import com.judy.jpa.chapter5.domain.Team;
+import jakarta.persistence.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
+@SpringBootApplication
 public class JpaMain {
 
     public static void main(String[] args) {
-        Member member1 = new Member("member1", "회원1");
-        Member member2 = new Member("member2", "회원2");
+        ApplicationContext context = SpringApplication.run(JpaMain.class, args);
+        EntityManagerFactory emf = context.getBean(EntityManagerFactory.class);
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+            testSave(em);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+    public static void testSave(EntityManager em) {
         Team team1 = new Team("team1", "팀1");
+        em.persist(team1);
 
+        Member member1 = new Member("member1", "회원1");
         member1.setTeam(team1);
-        member2.setTeam(team1);
+        em.persist(member1);
 
-        Team findTeam = member1.getTeam();
-        System.out.println(findTeam.getId());
-        System.out.println(findTeam.getName());
+        Member member2 = new Member("member2", "회원2");
+        member2.setTeam(team1);
+        em.persist(member2);
     }
 
 }
