d83d96e759237e66ab7d1dd494fc43410ec6b7db
Ara Jo <joara9566@naver.com>
2024-10-17 23:45:17 +0900

    연관관계의 주인

--- src/main/java/com/judy/jpa/chapter5/domain/Member.java
+++ src/main/java/com/judy/jpa/chapter5/domain/Member.java
@@ -19,7 +19,7 @@ public class Member {
 
     @ManyToOne
     @JoinColumn(name = "TEAM_ID")
-    private Team team;
+    private Team team; // Member - Team 사이에서 Member.team이 연관관계의 주인 -> 외래 키 관리를 여기에서 담당!
 
     public Member(String id, String username) {
         this.id = id;
